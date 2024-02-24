01a515e73ce530ead173224ee85853eab8f7e5c4
Dmitriy <dima951488@yanedx.ru>
2024-02-24 19:23:27 +0300

    code refactoring

--- src/components/ErrorBlock/ErrorBlock.jsx
+++ src/components/ErrorBlock/ErrorBlock.jsx
@@ -1,10 +1,10 @@
 import './ErrorBlock.css'
 
-export default function ErrorBlock({ onAnimationEnd }) {
+export default function ErrorBlock({ message, onAnimationEnd }) {
     return (
         <div className='error-block' onAnimationEnd={() => onAnimationEnd(false)}>
             <h3>Error</h3>
-            <div>Задача должна быть длиннее 3-х символов</div>
+            <div>{ message }</div>
         </div>
     )
 }
