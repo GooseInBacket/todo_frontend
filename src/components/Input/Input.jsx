4b054e9812b106ef41e09a439025f77a5ac9f657
Dmitriy <dima951488@yanedx.ru>
2024-02-24 19:22:08 +0300

    code refactoring

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -10,8 +10,10 @@ export default function Input({id, type, placeholder, queryHandler, value, onKey
             id={id} 
             name='input-data'
             value={value} 
-            onChange={(e) => queryHandler(e.target.value)} 
-            onKeyDown={(e) => onKeyDown(e)} 
+            onChange={(e) => {queryHandler(e.target.value)}} 
+            onKeyDown={(e) => onKeyDown(e)}
+            minLength={3} 
+            maxLength={256}
             autoFocus
         />
     )
